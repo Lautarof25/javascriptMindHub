22befaa269df9426f4cc8c4357bc6e2d7359641f
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-02-27 20:14:58 -0300

    agregando ejercicio de la clase

---
+++ Ejercicio-27-02/ejercicio.js
@@ -0,0 +1,37 @@
+// Datos básicos:
+
+// objeto datos basicos (nombre, apellido, edad, etc y debe tener un array hobbies)
+
+// objeto vehiculo con minimo 3 propiedades
+
+// objeto vivienda con minimo 8 propiedades
+
+// unir 3 objetos en un array
+
+var datos_persona = {
+    nombre: "Lautaro",
+    apellido: "Fernández",
+    edad: 33,
+    en_pareja: true,
+    hijos: 0,
+    pasatiempos: ["Ping pong","Programar","Andar en bicicleta"]
+}
+
+var vehiculo = {
+    marca: "Fiat",
+    modelo: "Palio",
+    anio: 2015,
+    color: "Gris",
+    puertas: 3
+}
+
+var vivienda = {
+    ubicacion: "Hurlingham",
+    tipo: "Casa",
+    cantidad_habitaciones: 5,
+    anios_antiguedad: 50,
+    metros_cuadrados: 80,
+    con_patio: true,
+    con_garaje: true,
+    terraza_accesible: true,
+}

e6e2b48ac8f7419a67f5f67c5aa9e62c48bc9de0
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-02 23:16:05 -0300

    ejercicio funciones

--- 2_Access_and_Display_Data/Ejercicios/script.js
+++ 2_Access_and_Display_Data/Ejercicios/script.js
@@ -60,3 +60,64 @@ while(i < arrayLenguajes.length){
 for (props in celular){
     console.log(props +": "+celular[props] )
 }
+
+// Ejercicio Funciones 
+
+const nombre = "Lautaro";
+const apellido = "Fernández";
+let edad = 33;
+const dni = 35174222;
+let domicilio = "Hurlingham";
+
+function imprimir_datos(){
+    console.log("Hola mi nombre es",nombre,apellido,"tengo",edad,"mi dni es",dni,"y vivo en",domicilio)
+}
+imprimir_datos();
+
+function calcularIva(precio){
+    var iva = 21;
+    var total = precio +(iva * precio / 100);
+    console.log("El producto con precio",precio,"cuesta en total con iva",total)
+}
+
+calcularIva(100);
+
+var arrayDeNumeros = [1,2,3,4,5];
+
+function sumarArrays(array){
+    var totalArray = 0;
+    for (let i = 0; i <= array.length; i++) {
+        totalArray = i + totalArray;
+    }
+    console.log("El total de los numeros sumamos en el array es",totalArray)
+}
+
+sumarArrays(arrayDeNumeros);
+
+function alertaEdadConducir(unaEdad){
+    if(unaEdad >= 18){
+        alert("ya puedes conducir")
+    }else if (unaEdad < 18){
+        alert("Aun no puedes conducir")
+    }
+}
+
+// alertaEdadConducir(16)
+
+// Ejercicio DOM 
+
+document.getElementById("nombre").innerHTML = nombre;
+document.getElementById("apellido").innerHTML = apellido;
+document.getElementById("edad").innerHTML = edad;
+
+document.querySelector("#nombre").innerHTML = "otro nombre";
+document.querySelector("#apellido").innerHTML = "otro apellido";
+document.querySelector("#edad").innerHTML = "otra edad";
+
+var claseVar = document.getElementsByClassName("nombre");
+
+var tagVar = document.getElementsByTagName("h3");
+
+console.log(claseVar);
+
+

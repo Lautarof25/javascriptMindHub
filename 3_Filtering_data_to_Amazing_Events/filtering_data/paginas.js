4cbb061ce6acc58dd830128ac8ec669cd4d88ea5
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-08 20:50:51 -0300

    cambios generales

---
+++ 3_Filtering_data_to_Amazing_Events/filtering_data/paginas.js
@@ -0,0 +1,20 @@
+var dataEvents;
+var dataBase;
+var page = "Home";
+var content;
+
+switch(page){
+    case "UpcomingEvents":
+        break;
+    case "PastEvents":
+
+        break;
+    case "Contact":
+
+        break;
+    case "Stats":
+
+        break;
+    default:
+
+}

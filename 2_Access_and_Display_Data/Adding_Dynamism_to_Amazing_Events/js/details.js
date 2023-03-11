3b2348f9850a81903e8503c3b81e55272d7d5d22
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-11 14:40:24 -0300

    copying date

---
+++ 2_Access_and_Display_Data/Adding_Dynamism_to_Amazing_Events/js/details.js
@@ -0,0 +1,3 @@
+import myJson from "../AmazingEvents.json" assert {type:"json"};
+
+console.log(myJson)
--- 2_Access_and_Display_Data/Adding_Dynamism_to_Amazing_Events/js/navbar.js
+++ 2_Access_and_Display_Data/Adding_Dynamism_to_Amazing_Events/js/navbar.js
@@ -2,13 +2,16 @@
 const navToggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav-menu");
 const navMenuAll = document.querySelectorAll(".nav-menu");
+const img_detail = document.querySelector(".img_detail");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+    img_detail.style.zIndex = "-1";
     if (navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label", "cerrar menú");
     } else {
         navToggle.setAttribute("aria-label", "abrir menú");
+        img_detail.style.zIndex = "0";
     }
 });
 

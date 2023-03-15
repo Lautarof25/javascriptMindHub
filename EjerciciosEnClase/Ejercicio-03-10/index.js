6e7d567b3a5d02c6dcf6b8d80c535114f3daed94
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-15 14:05:40 -0300

    subiendo ejercios terminados de la practica 10-03

--- EjerciciosEnClase/Ejercicio-03-10/index.js
+++ EjerciciosEnClase/Ejercicio-03-10/index.js
@@ -170,14 +170,41 @@ Crear un array llamado familia con un mínimo de 5 objetos y mostrar por consola
 
 */
 
+const familia = [
+    lautaro = {
+        edad: 33
+    },
+    ivan = {
+        edad: 31
+    },
+    romina = {
+        edad: 41
+    },
+    felipe = {
+        edad: 1
+    },
+    jorge = {
+        edad: 71
+    }
+]
 
+console.log(familia);
 
+console.log(familia[0])
+console.log(familia[1])
+console.log(familia[2])
+console.log(familia[3])
+console.log(familia[4])
 
 /*
 Crear una variable llamada textoAleatorio formando una frase con el segundo elemento del array del punto 13, el cuarto elemento del punto 14 y el quinto objeto del array del punto 15.
 
 */
 
+
+const textoAleatorio = frutas[2]+ numeros[3] + familia[4].edad;
+
+console.log(textoAleatorio);
 
 /*
 Utilizar prompt() para leer por pantalla mi edad y la edad de un compañero y mostrar por consola los resultados de comparar los valores y guardarlos en variables llamadas por ejemplo: edadesIguales, soyMayor, soyMenor, etc. Y mostrar mensajes en consola como los siguientes:
@@ -187,6 +214,16 @@ Mi edad es menor a la de mi compañero: false
 
 */
 
+// const edadPrompt = prompt("Ingrese su edad ");
+// const edadCompanero = prompt("Ingrese la edad de su compañero ");
+
+// const edadesIguales = edadPrompt == edadCompanero;
+// const soyMayor = edadPrompt > edadCompanero;
+// const soyMenor = edadPrompt < edadCompanero;
+
+// console.log("Mi edad es igual a la de mi compañero: "+edadesIguales);
+// console.log("Mi edad es mayor a la de mi compañero: "+soyMayor);
+// console.log("Mi edad es menor a la de mi compañero: "+soyMenor);
 
 /*
 
@@ -194,9 +231,34 @@ Compare su edad ingresada por pantalla con prompt() con el numero 18 y guardarlo
 
 */
 
+// const soyMayorDeEdad = edadPrompt >= 18;
+
+// console.log("Soy mayor de edad: "+soyMayorDeEdad)
 
 /*
 Introducir por pantalla la edad y la altura de una persona y guardarlas en variables separadas y en una variable llamada puedeSubir el resultado de la operación resultante de si la persona es mayor de 6 años y además tiene una altura mínima de 120cm y mostrar por consola un mensaje como el siguiente: Puede subir a la atracción y el valor de la variable resultante.
-Introducir por pantalla el pase de una persona el cual puede ser “VIP”, “NORMAL” o “LIMITADO”, el saldo que dispone y guardarlos en variables separadas. En una variable llamada puedePasar guardar el resultado de la operación resultante de si la persona tiene pase “VIP” o si posee un saldo mayor a 1000. Mostrando un mensaje que diga: La persona puede pasar y el resultado de la variable.
 
 */
+
+// const edadPersona = prompt("Ingrese la edad de la persona")
+// const estaturaPersona = prompt("Ingrese la estatura de la persona en cm")
+
+// const puedeSubir = edadPersona > 6 && estaturaPersona > 120;
+
+// console.log("Puede subir a la atracción "+puedeSubir)
+
+
+/*
+
+Introducir por pantalla el pase de una persona el cual puede ser “VIP”, “NORMAL” o “LIMITADO”, el saldo que dispone y guardarlos en variables separadas. 
+En una variable llamada puedePasar guardar el resultado de la operación resultante de si la persona tiene pase “VIP” o si posee un saldo mayor a 1000. Mostrando un mensaje que diga: La persona puede pasar y el resultado de la variable.
+
+*/
+
+const pasePersona = prompt("Ingrese el pase : VIP - NORMAL - LIMITADO").toUpperCase();
+
+const saldoDisposible = Number(prompt("Ingrese el saldo"))
+
+const puedePasar = pasePersona == "VIP" && saldoDisposible > 1000;
+
+console.log("La persona puede pasar: "+ puedePasar)

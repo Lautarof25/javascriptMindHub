b96d70839e72e07339d42f7bb56698411a8d935a
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-16 12:16:02 -0300

    agregando input search

--- 3_Filtering_data_to_Amazing_Events/filtering_data/js/index.js
+++ 3_Filtering_data_to_Amazing_Events/filtering_data/js/index.js
@@ -12,71 +12,63 @@ const all_checked = document.querySelector("#all");
 
 // Creamos un evento del formulario
 var form = document.querySelector("form");
+
+// ------------- Busqueda por checkboxes ---------------------
+
 // Cada vez que ocurra un evento change, se autoejecuta 
-form.addEventListener("change", () => { formData() })
+form.addEventListener("click", () => { formData() })
 
 function formData() {
     // Obtengo el checkbox
     let checkboxes = document.querySelectorAll("input[type=checkbox]");
     // Creo el array para guardar los chequedado
-    let checked = []
+    let checkeds = []
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
-            checked.push(checkboxes[i].value)
+            checkeds.push(checkboxes[i].value)
         }
     }
-    if (checked.length > 1 && all_checked.checked) {
-        all_checked.checked = false;
+    // Si "all" está tildado y tildo otra categoria
+    if (all_checked.checked && checkeds.length > 1) {
+            // "all" se destilda
+            all_checked.checked = false;
     }
-    printEvents(checked)
+    // Ejecuto la impresión de eventos en base a los elementos tildados
+    printEvents(checkeds)
+}
+
+function unckeckAll(arrayChecks){
+    for (let i = 1; i < arrayChecks.length; i++) {
+        arrayChecks[i].checked = false;
+    }
 }
 
 // Imprimo las tarjetas - Lo pongo dentro de una función
 function printEvents(chk) {
-    if (all_checked.checked) {
-        for (let j = 0; j < eventos.length; j++) {
+    // Reseteamos el HTML
+    cards.innerHTML = `
+                `
+    // Por cada elemento de los eventos
+    for (let j = 0; j < eventos.length; j++) {
+        // Si el array checked tiene el evento en cuestión, lo imprime
+        if (chk.includes(eventos[j].category)) {
             cards.innerHTML += `
-             <div class="card">
-                 <div class="img">
-                      <img src="./Images/${getFinalUrlImage(j)}" alt="service">
+                 <div class="card">
+                     <div class="img">
+                          <img src="./Images/${getFinalUrlImage(j)}" alt="service">
+                     </div>
+                     <h2>${eventos[j].name}</h2>
+                     <p>${eventos[j].description}
+                     </p>
+                     <div>
+                         <p>Price: ${eventos[j].price}</p>
+                         <a href="./pages/details.html">see more...</a>
+                     </div>
                  </div>
-                 <h2>${eventos[j].name}</h2>
-                 <p>${eventos[j].description}
-                 </p>
-                 <div>
-                     <p>Price: ${eventos[j].price}</p>
-                     <a href="./pages/details.html">see more...</a>
-                 </div>
-             </div>
-         `
-        }
-    } else {
-        // Reseteamos el HTML
-        cards.innerHTML = `
-            `
-        // Por cada elemento de los eventos
-        for (let j = 0; j < eventos.length; j++) {
-            // Si el array checked tiene el evento en cuestión, lo imprime
-            if (chk.includes(eventos[j].category)) {
-                cards.innerHTML += `
-             <div class="card">
-                 <div class="img">
-                      <img src="./Images/${getFinalUrlImage(j)}" alt="service">
-                 </div>
-                 <h2>${eventos[j].name}</h2>
-                 <p>${eventos[j].description}
-                 </p>
-                 <div>
-                     <p>Price: ${eventos[j].price}</p>
-                     <a href="./pages/details.html">see more...</a>
-                 </div>
-             </div>
-         `
-            }
+             `
         }
     }
 }
-
 
 // Obtener La parte final de la propiedad image para la imagen
 function getFinalUrlImage(id) {
@@ -101,3 +93,17 @@ function reverseUrlImagen(url) {
     }
     return urlFinalReverse;
 }
+
+//  ------------- Busqueda por input search ------------------
+
+let dataFromForm;
+// Obtengo el input search
+var input = document.querySelector("input[type=search]")
+
+input.addEventListener("change", (evento) => {
+    evento.preventDefault();
+    dataFromForm = {
+        inputSearch: evento.target.value
+    }
+    console.log(dataFromForm.inputSearch)
+});

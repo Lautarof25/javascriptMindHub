40e4aaa91259373bc7f7dd1b1a4c384bc3c9c0d4
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-16 22:05:47 -0300

    ejercicios condicional

--- EjerciciosEnClase/Ejercicio-03-16/index.js
+++ EjerciciosEnClase/Ejercicio-03-16/index.js
@@ -56,7 +56,7 @@ Realizar un programa que dado 3 números imprima por pantalla cual de los 3 es e
 Realizar un programa que dado 2 objetos representando personas con las propiedades 
 nombre, edad y altura imprima por pantalla cual de las 2 personas es la mas alta y cual es la de mayor edad.
 */
-const persona = {
+let persona = {
     nombre: "nombre",
     edad: 99,
     altura: 1.55
@@ -95,13 +95,139 @@ if(persona1.edad > 18 && persona2.edad > 18){
 
 Realizar un programa que permita ingresar por pantalla tu nombre, edad, altura, visión y permita determinar si estas capacitado para conducir. La persona deberá cumplir con una edad mínima de 18 años, medir mas de 150cm y tener una visión de 8 de 10 como mínimo.
 
+*/
+
+    const persona3 = new Object(persona)
+
+    persona3.nombre = prompt("Ingrese su nombre");
+    persona3.edad = prompt("Ingrese su edad");
+    persona3.altura = prompt("Ingrese su altura");
+    persona3.vision = Number(prompt("Ingrese su vision"));
+
+
+    if (persona3.edad >= 18 && persona3.vision >= 8){
+        console.log("Estás capacitado para conducir")
+    }else {
+        console.log("No Estás capacitado para conducir")
+    }
+
+/*
+
 Realizar un programa que permita el ingreso de los siguientes datos: Nombre, pase (vip o normal), si posee entrada (si o no, s o n, true o false). Las personas que posean tu mismo nombre tienen ingreso libre así como también los que posean un pase vip, mostrar un mensaje de bienvenida. Si posee entrada preguntar si desea utilizarla, en caso afirmativo mostrar mismo mensaje de bienvenida. Y por ultimo de no tener el mismo nombre o poseer un pase vip o entrada, preguntar si desea comprar, caso afirmativo solicitar dinero disponible, si posee $1000 o mas, mostrar mensaje de venta de entrada y bienvenida, caso contrario mostrar mensaje de rechazo de venta, y en caso contrario a no querer comprar, mostrar mensaje de despedida.
+*/
+    
+    const nombrePersona = prompt("Ingresar nombre ");
+    const pase = prompt("Ingresar 1 - Vip | 2 - Normal");
+    const entrada = prompt("Tiene entrada: 1 - Si | 2 - No ");
+    let comprar;
+    let saldo = 1500;
+
+    if(nombrePersona == "Lautaro" || pase == "1"){
+        console.log("Bienvenido!")
+    }else if (entrada == "1"){
+        console.log("Bienvenido!")
+    }else {
+        comprar = prompt("Desea comprar entrada? 1 - Si | 2 - No")
+        if(comprar == "1"){
+            if(saldo >= 1000){
+                console.log("Entrada vendida correctamente")
+                console.log("Bienvenido!")
+            }else {
+                console.log("Venta rechazada -  Posee menos de 1000")
+            }
+        }else {
+            console.log("Gracias por utilizar el programa")
+        }
+    }
+
+/*
 Realizar un juego de adivinanza estableciendo un valor entre 1 y 10 en una variable llamada numeroIncognita y que permita en 3 intentos adivinar el numero. El usuario deberá ingresar un numero del 1 al 10 por pantalla en 3 intentos el cual se deberá guardar en una variable llamada numeroIngresado, y en cada intento deberás mostrarle un mensaje al usuario diciendo: “el numero ingresado es mayor, vuelve a intentarlo” o “el numero ingresado es menor, vuelve a intentarlo” o en caso de adivinar, un mensaje que diga: “Ganaste, haz adivinado el numero.” No te preocupes si usas mucho código repetido, mas adelante veraz como realizar este juego de manera mas eficiente.
+*/
+
+const numeroIncognita = 9;
+
+let numeroIngresado = prompt("Ingrese un numero del 1 al 10");
+
+const intentos = 3;
+
+if(numeroIngresado > numeroIncognita && intentos > 0){
+    console.log("El numero ingresado es mayor, vuelva a intentarlo")
+    intentos--;
+}else if(numeroIngresado < numeroIncognita && intentos > 0){
+    console.log("El numero ingresado es menor, vuelva a intentarlo")
+    intentos--;
+}else if(numeroIngresado == numeroIncognita){
+    console.log("Ganaste, haz adivinado el numero")
+}
+
+numeroIngresado = prompt("Ingrese un numero del 1 al 10");
+
+if(numeroIngresado > numeroIncognita && intentos > 0){
+    console.log("El numero ingresado es mayor, vuelva a intentarlo")
+    intentos--;
+}else if(numeroIngresado < numeroIncognita && intentos > 0){
+    console.log("El numero ingresado es menor, vuelva a intentarlo")
+    intentos--;
+}else if(numeroIngresado == numeroIncognita){
+    console.log("Ganaste, haz adivinado el numero")
+}
+
+numeroIngresado = prompt("Ingrese un numero del 1 al 10");
+
+if(numeroIngresado > numeroIncognita && intentos > 0){
+    console.log("El numero ingresado es mayor, vuelva a intentarlo")
+    intentos--;
+}else if(numeroIngresado < numeroIncognita && intentos > 0){
+    console.log("El numero ingresado es menor, vuelva a intentarlo")
+    intentos--;
+}else if(numeroIngresado == numeroIncognita){
+    console.log("Ganaste, haz adivinado el numero")
+}else {
+    console.log("Perdiste")   
+}
+
+/*
 Crear un programa que permita ingresar tu edad y decir si eres un infante (0 a 12 años), adolescente(13 a 18 años), un mayor joven (19 a 45 años) o un anciano (mas de 45 años), y en caso de ingresar una edad mayor a 100 mostrar un mensaje preguntando si en realidad tiene esa edad.
+*/
+
+const edadOtra = prompt("Ingresa tu edad")
+
+if(edadOtra >= 0 && edadOtra <= 12){
+    console.log("Eres un infante")
+}else if(edadOtra > 12 && edadOtra <= 18){
+    console.log("Eres un adoleescente")
+}else if(edadOtra > 18 && edadOtra <= 45){
+    console.log("Eres un mayor joven")
+}else if(edadOtra > 45 && edadOtra <= 100){
+    console.log("Eres un anciano")
+}else if(edadOtra > 100){
+    console.log("En realidad tienes esa edad")
+}
+
+
+/*
 Crear un programa que permita el ingreso de “PIEDRA”, “PAPEL” o “TIJERAS” a 2 jugadores y muestre en pantalla cual de los 2 ha ganado o si han empatado. En caso de algún ingreso incorrecto mostrar por pantalla que uno de los jugadores ha hecho trampa.
+*/
+
+
+
+/*
 Realizar un programa que permita el ingreso de un color y utilizando “switch” mostrar por pantalla los siguientes mensajes según las opciones: Blanco o Negro: Falta de color, Verde: El color de la naturaleza, Azul: El color del agua, Amarillo: El color del sol, Rojo: El color del fuego, Marrón: el color de la tierra, y para cualquier otro valor: Excelente elección, no lo teníamos pensado.
+*/
+
+
+
+/*
 Realizar un programa que permita el ingreso de 2 valores numéricos y una operación. Según sea la operación ingresada (suma, resta, multiplicación, división) el programa deberá mostrar en pantalla un mensaje junto con el resultado. En caso de haber elegido división realizar la operación siempre que sea posible o mostrar un mensaje de ERROR si el divisor ingresado fue 0.
+*/
+
+
+
+/*
 Crear un programa que permita ingresar todos los datos de tu documento nacional de identidad, mostrar por pantalla un mensaje que imprima todos los datos ingresados y 
 pregunte si están correctos los mismos. En caso afirmativo, crear un objeto llamado dni con todos los datos ingresados y mostrarlos por consola con console.table() mas un mensaje de registro exitoso, en caso de que la persona rechace confirmar los datos, mostrar un mensaje que diga: vuelva a intentarlo en 1 mes.
+*/
+
+/*
 
 */

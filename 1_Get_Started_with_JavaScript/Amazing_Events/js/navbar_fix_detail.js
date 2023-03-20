6c3206cc9defbd092d6105921c211f558c345ec2
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-03-20 19:39:40 -0300

    corrección de checkboxes categories

--- 1_Get_Started_with_JavaScript/Amazing_Events/js/navbar_fix_detail.js
+++ 1_Get_Started_with_JavaScript/Amazing_Events/js/navbar_fix_detail.js
@@ -6,10 +6,12 @@ const img_detail = document.querySelector(".img_detail");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+    img_detail.style.zIndex = "-1";
     if (navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label", "cerrar menú");
     } else {
         navToggle.setAttribute("aria-label", "abrir menú");
+        img_detail.style.zIndex = "0";
     }
 });
 
